71468a44cee47c107e250d5339c6be98e2496122
RayyanMFPP <rayyantelyu@gmail.com>
2026-04-05 21:35:52 +0700

    pengerjaan sayaMusicTrack

--- TP_Modul6_103022400121/Program.cs
+++ TP_Modul6_103022400121/Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.Contracts;
+
+class SayaMusicTrack
+{
+    private int id;
+    private string playcount;
+    private string title;
+
+    public SayaMusicTrack(string title)
+    {
+        Random random = new Random();
+        this.playcount = "0";
+        this.id = random.Next(10000, 100000);
+        this.title = title;
+    }
+
+    public void increasePlayCount(string angka)
+    {
+        this.playcount = angka;
+    }
+    
+    public void printTrackDetails()
+    {
+        Console.WriteLine("ID: " + this.id);
+        Console.WriteLine("Title: " + this.title);
+        Console.WriteLine("Play Count: " + this.playcount);
+    }
+}
+
+class main
+{
+    public static void Main(string[] args)
+    {
+        SayaMusicTrack track1 = new SayaMusicTrack("Lagu Pertama");
+        track1.increasePlayCount("100");
+        track1.printTrackDetails();
+        Console.WriteLine();
+        SayaMusicTrack track2 = new SayaMusicTrack("Lagu Kedua");
+        track2.increasePlayCount("250");
+        track2.printTrackDetails();
+    }
+}

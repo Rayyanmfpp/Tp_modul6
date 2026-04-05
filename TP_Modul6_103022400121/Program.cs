ee6ddbe3a0a6211d9a952f8a415b6347aecf51a8
RayyanMFPP <rayyantelyu@gmail.com>
2026-04-05 22:32:34 +0700

    last commit

--- TP_Modul6_103022400121/Program.cs
+++ TP_Modul6_103022400121/Program.cs
@@ -1,42 +1,59 @@
-﻿using System.Diagnostics.Contracts;
+﻿using System.Diagnostics;
 
 class SayaMusicTrack
 {
     private int id;
-    private string playcount;
+    private string playCount;
     private string title;
 
     public SayaMusicTrack(string title)
     {
+        Debug.Assert(title != null, "Judul tidak boleh null!");
+        Debug.Assert(title.Length <= 100, "Judul maksimal 100 karakter!");
         Random random = new Random();
-        this.playcount = "0";
+        this.playCount = "0";
         this.id = random.Next(10000, 100000);
         this.title = title;
     }
 
-    public void increasePlayCount(string angka)
+    public void increasePlayCount(int count)
     {
-        this.playcount = angka;
-    }
-    
+        try
+        {
+            int currentCount = int.Parse(this.playCount);
+
+            checked
+            {
+                int updatedCount = currentCount + count;
+                this.playCount = updatedCount.ToString();
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Play count melebihi batas maksimum integer (Overflow)!");
+        }
+    }    
     public void printTrackDetails()
     {
         Console.WriteLine("ID: " + this.id);
         Console.WriteLine("Title: " + this.title);
-        Console.WriteLine("Play Count: " + this.playcount);
+        Console.WriteLine("Play Count: " + this.playCount);
     }
 }
 
 class main
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
-        SayaMusicTrack track1 = new SayaMusicTrack("Lagu Pertama");
-        track1.increasePlayCount("100");
-        track1.printTrackDetails();
-        Console.WriteLine();
-        SayaMusicTrack track2 = new SayaMusicTrack("Lagu Kedua");
-        track2.increasePlayCount("250");
-        track2.printTrackDetails();
+        SayaMusicTrack lagu = new SayaMusicTrack("Lagu Kece");
+        Console.WriteLine("Memulai simulasi penambahan play count...");
+        lagu.printTrackDetails();
+
+        for (int i = 0; i < 100; i++)
+        {
+            lagu.increasePlayCount(10000000);
+        }
+
+        lagu.printTrackDetails();
     }
 }
